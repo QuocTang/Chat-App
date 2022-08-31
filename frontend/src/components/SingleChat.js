57ecb5c8b376650f69441bdc31b38a5a87d573c7
QuocTang <76061383+QuocTang@users.noreply.github.com>
2022-08-31 17:17:38 +0700

    Update SingleChat.js

--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -163,7 +163,7 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
       {selectedChat ? (
         <>
           <Text
-            fontSize={{ base: "28px", md: "30px" }}
+            fontSize={{ base: "20px", md: "30px" }}
             pb={3}
             px={2}
             w="100%"
